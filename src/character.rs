e0defd50b8be79a26a99f04763f3093eadc78287
nacir1i <taoufiqnaciri234@gmail.com>
2024-01-12 15:52:19 +0100

    implement basic kinematic controller for character

--- src/character.rs
+++ src/character.rs
@@ -1,12 +1,11 @@
 use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
 use leafwing_input_manager::{prelude::*, user_input::InputKind};
 
 use crate::assets_loader::SceneAssets;
-use crate::movement::{Acceleration, MovingObjectBundle, Velocity};
+use crate::plane::Gravity;
 
-const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, 0.0);
-const SPEED: f32 = 5.0;
-const ROTATION_SPEED: f32 = 2.5;
+const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 2.0, 0.0);
 
 #[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug, Reflect)]
 pub enum PlayerAction {
@@ -56,57 +55,22 @@ fn spawn_character(mut commands: Commands, scene_assets: Res<SceneAssets>) {
     }
 
     commands.spawn((
-        MovingObjectBundle {
-            velocity: Velocity::new(Vec3::ZERO),
-            acceleration: Acceleration::new(Vec3::ZERO),
-            model: SceneBundle {
-                scene: scene_assets.character.clone(),
-                transform: Transform::from_translation(STARTING_TRANSLATION),
-                ..default()
-            },
+        SceneBundle {
+            scene: scene_assets.character.clone(),
+            transform: Transform::from_translation(STARTING_TRANSLATION),
+            ..default()
         },
+        RigidBody::KinematicPositionBased,
+        KinematicCharacterController::default(),
+        Collider::ball(0.5),
+        Restitution::coefficient(0.7),
         InputManagerBundle::<PlayerAction> {
             input_map,
             ..default()
         },
         Character,
+        Gravity,
     ));
 }
 
-fn character_movement_controls(
-    mut query: Query<
-        (
-            &mut Transform,
-            &mut Velocity,
-            &mut ActionState<PlayerAction>,
-        ),
-        With<Character>,
-    >,
-    keyboard_input: Res<Input<KeyCode>>,
-    time: Res<Time>,
-) {
-    let (mut transform, mut velocity, input) = query.single_mut();
-
-    let mut rotation = 0.0;
-    let mut movement = 0.0;
-
-    if input.pressed(PlayerAction::Run) {
-        println!("Running");
-    }
-
-    if keyboard_input.pressed(KeyCode::W) {
-        movement = SPEED;
-    } else if keyboard_input.pressed(KeyCode::S) {
-        movement = -SPEED;
-    }
-
-    if keyboard_input.pressed(KeyCode::D) {
-        rotation = -ROTATION_SPEED * time.delta_seconds();
-    } else if keyboard_input.pressed(KeyCode::A) {
-        rotation = ROTATION_SPEED * time.delta_seconds();
-    }
-
-    transform.rotate_y(rotation);
-
-    velocity.value = -transform.forward() * movement;
-}
+fn character_movement_controls() {}
